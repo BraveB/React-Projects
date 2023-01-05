4318d2fda0bdc065479612fcd55ff26fc36d0a48
Beatriz Bravo Gallardo <beatriz.bravo@globant.com>
2023-01-05 17:41:50 -0600

    task/328-bar-charts-data

--- github-search/src/components/Charts/Bar3D.js
+++ github-search/src/components/Charts/Bar3D.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const Bar3D = () => {
-  return <div>chart</div>;
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "bar3d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Forked",
+        yAxisName: "Forks",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "1rem",
+        yAxisNameFontSize: "1rem",
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Bar3D;
+export default ChartComponent;
--- github-search/src/components/Charts/Column3D.js
+++ github-search/src/components/Charts/Column3D.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const Column3D = () => {
-  return <div>chart</div>;
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Popular",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "1rem",
+        yAxisNameFontSize: "1rem",
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
