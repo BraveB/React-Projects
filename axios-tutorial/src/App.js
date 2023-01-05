0b65803d449300fdd2afc4cdefdad28e1f355ea0
Beatriz Bravo Gallardo <beatriz.bravo@globant.com>
2023-01-05 12:42:33 -0600

    task/custom-instance

--- axios-tutorial/src/App.js
+++ axios-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Title from "./components/Title";
-import Setup from "./examples/4-global-instance";
+import Setup from "./examples/5-custom-instance";
 import axios from "./axios/global";
 
 function App() {
