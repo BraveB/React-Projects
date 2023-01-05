1166de9a73f919be54d122c0b86e3ecaa715fffd
Beatriz Bravo Gallardo <beatriz.bravo@globant.com>
2023-01-05 17:10:10 -0600

    task/326-doughnut-chart-data

--- github-search/src/components/Repos.js
+++ github-search/src/components/Repos.js
@@ -4,28 +4,38 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = useContext(GithubContext);
-  let languanges = repos.reduce((total, item) => {
-    const { language } = item;
+  const languanges = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     if (!language) return total;
-    if (!total[language]) total[language] = { label: language, value: 1 };
+    if (!total[language])
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     else
       total[language] = total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     return total;
   }, {});
-  languanges = Object.values(languanges)
+  const mostUsed = Object.values(languanges)
     .sort((a, b) => {
       return b.value - a.value;
+    })
+    .slice(0, 5);
+  const mostPopular = Object.values(languanges)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
     })
     .slice(0, 5);
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={languanges} />
+        <Pie3D data={mostUsed} />
         <div></div>
-        <Doughnut2D data={languanges} />
+        <Doughnut2D data={mostPopular} />
       </Wrapper>
     </section>
   );
