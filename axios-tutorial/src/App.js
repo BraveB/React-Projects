5bb7df008068f87d00acc8459307b9d831b14265
Beatriz Bravo Gallardo <beatriz.bravo@globant.com>
2023-01-05 12:12:07 -0600

    task/first-request

---
+++ axios-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./components/Title";
+import Setup from "./examples/1-first-request";
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
